de65a638c8b7bc6b36e5a3fd22ec2458d37f7ad9
Sarvajeet <sarvajeetnarkhede5072@gmail.com>
2024-08-23 15:02:43 +0530

    First

--- Main.java
+++ Main.java
@@ -2,10 +2,7 @@ import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        Scanner scanner =new Scanner(System.in);
-        System.out.println("Enter age ");
-        int age=scanner.nextInt();
-        System.out.println("My age is " + age);
+        System.out.println("Hi");
 
     }
 }
